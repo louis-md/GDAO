dd584dd14b0bce395b62e3f652a6ebb39d35ecd1
Janaka-Steph <rochestephane@yahoo.fr>
2017-04-21 11:35:26 +0200

    fix test normcorpus

--- test/javascript/normcorpus_spec.js
+++ test/javascript/normcorpus_spec.js
@@ -1,6 +1,6 @@
 let NormCorpusAbstraction = artifacts.require("NormCorpus");
 let NormCorpus = null;
-let contractAddress = '0x3b4a55dd0926b8048266f17ed507907f1a2c1988';
+let contractAddress = '0xdeadbeef';
 
 let beforeWrapper = function(accounts) {
   before(async () => {
@@ -18,15 +18,16 @@ contract('NormCorpus', (accounts) => {
   // runs before all tests in this block
   beforeWrapper(accounts);
 
-  it.only("should insert a contract into the registry", async () => {
+  it("should insert a contract into the registry", async () => {
+    // deploy 3 insert Legislator to NormCorpus
     let numNorms = await NormCorpus.numberOfNorms.call();
-    assert.equal(numNorms.toNumber(), 0, "numberOfNorms should equal to 0");
+    assert.equal(numNorms.valueOf(), 1, "numberOfNorms should equal to 1");
     let txInsert = await NormCorpus.insert.sendTransaction(contractAddress);
     console.log(`Insert transaction ID: ${txInsert}`);
     let isNormBool = await NormCorpus.isNorm.call(contractAddress);
     assert.ok(isNormBool, 'Contract address should be truthy in isNorm registry');
     let numNorms2 = await NormCorpus.numberOfNorms.call();
-    assert.equal(numNorms2.toNumber(), 1, "numberOfNorms should equal to 1");
+    assert.equal(numNorms2.toNumber(), 2, "numberOfNorms should equal to 2");
   });
 });
 
@@ -50,12 +51,12 @@ contract('NormCorpus 3', (accounts) => {
   it("should remove the inserted contract from the registry", async () => {
     await NormCorpus.insert.sendTransaction(contractAddress);
     let numNorms = await NormCorpus.numberOfNorms.call();
-    assert.equal(numNorms.toNumber(), 1, "numberOfNorms should equal to 1");
+    assert.equal(numNorms.toNumber(), 2, "numberOfNorms should equal to 2");
     let txInsert = await NormCorpus.remove.sendTransaction(contractAddress);
     console.log(`Remove transaction ID: ${txInsert}`);
     let bool = await NormCorpus.isNorm.call(contractAddress);
-    assert.ok(!bool, 'Contract address should be falsy in isLegit registry');
+    assert.ok(!bool, 'Contract address should be falsy in isNorm registry');
     let numNorms2 = await NormCorpus.numberOfNorms.call();
-    assert.equal(numNorms2.toNumber(), 0, "numberOfNorms should equal to 0");
+    assert.equal(numNorms2.toNumber(), 1, "numberOfNorms should equal to 1");
   });
 });
