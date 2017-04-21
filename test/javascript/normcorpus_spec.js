977a6b71e4703c89ee3f54236e9644eb44fee8b9
Janaka-Steph <rochestephane@yahoo.fr>
2017-04-21 11:05:12 +0200

    refactor test normcorpus

--- test/javascript/normcorpus_spec.js
+++ test/javascript/normcorpus_spec.js
@@ -1,5 +1,5 @@
-let NormCorpus = artifacts.require("./NormCorpus.sol");
-let instance = {};
+let NormCorpusAbstraction = artifacts.require("NormCorpus");
+let NormCorpus = null;
 let contractAddress = '0x3b4a55dd0926b8048266f17ed507907f1a2c1988';
 
 let beforeWrapper = function(accounts) {
@@ -9,9 +9,8 @@ let beforeWrapper = function(accounts) {
     //web3.eth.default = web3.eth.accounts[0];
     //console.log('web3.eth.default', web3.eth.default);
 
-    // Put the contract instance in a variable
-    let res = await NormCorpus.deployed();
-    instance = res;
+    // Put the contract NormCorpus in a variable
+    NormCorpus = await NormCorpusAbstraction.deployed();
   });
 };
 
@@ -19,14 +18,14 @@ contract('NormCorpus', (accounts) => {
   // runs before all tests in this block
   beforeWrapper(accounts);
 
-  it("should insert a contract into the registry", async () => {
-    let numNorms = await instance.numberOfNorms.call();
+  it.only("should insert a contract into the registry", async () => {
+    let numNorms = await NormCorpus.numberOfNorms.call();
     assert.equal(numNorms.toNumber(), 0, "numberOfNorms should equal to 0");
-    let txInsert = await instance.insert.sendTransaction(contractAddress);
+    let txInsert = await NormCorpus.insert.sendTransaction(contractAddress);
     console.log(`Insert transaction ID: ${txInsert}`);
-    let isNormBool = await instance.isNorm.call(contractAddress);
+    let isNormBool = await NormCorpus.isNorm.call(contractAddress);
     assert.ok(isNormBool, 'Contract address should be truthy in isNorm registry');
-    let numNorms2 = await instance.numberOfNorms.call();
+    let numNorms2 = await NormCorpus.numberOfNorms.call();
     assert.equal(numNorms2.toNumber(), 1, "numberOfNorms should equal to 1");
   });
 });
@@ -37,8 +36,8 @@ contract('NormCorpus 2', (accounts) => {
   beforeWrapper(accounts);
 
   it('should contain the contract address', async () => {
-    await instance.insert.sendTransaction(contractAddress);
-    let bool = await instance.contains.call(contractAddress);
+    await NormCorpus.insert.sendTransaction(contractAddress);
+    let bool = await NormCorpus.contains.call(contractAddress);
     assert.ok(bool, 'should contain the inserted contract');
   });
 });
@@ -49,14 +48,14 @@ contract('NormCorpus 3', (accounts) => {
   beforeWrapper(accounts);
 
   it("should remove the inserted contract from the registry", async () => {
-    await instance.insert.sendTransaction(contractAddress);
-    let numNorms = await instance.numberOfNorms.call();
+    await NormCorpus.insert.sendTransaction(contractAddress);
+    let numNorms = await NormCorpus.numberOfNorms.call();
     assert.equal(numNorms.toNumber(), 1, "numberOfNorms should equal to 1");
-    let txInsert = await instance.remove.sendTransaction(contractAddress);
+    let txInsert = await NormCorpus.remove.sendTransaction(contractAddress);
     console.log(`Remove transaction ID: ${txInsert}`);
-    let bool = await instance.isNorm.call(contractAddress);
+    let bool = await NormCorpus.isNorm.call(contractAddress);
     assert.ok(!bool, 'Contract address should be falsy in isLegit registry');
-    let numNorms2 = await instance.numberOfNorms.call();
+    let numNorms2 = await NormCorpus.numberOfNorms.call();
     assert.equal(numNorms2.toNumber(), 0, "numberOfNorms should equal to 0");
   });
 });
