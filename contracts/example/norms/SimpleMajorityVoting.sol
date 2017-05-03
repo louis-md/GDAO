6e5648e9a4d47b739ee999cc80ec4cfd4150654b
AK42 <antokop@hotmail.com>
2017-05-03 16:35:10 +0200

    Added Simple Majority Explanation

--- contracts/example/norms/SimpleMajorityVoting.sol
+++ contracts/example/norms/SimpleMajorityVoting.sol
@@ -5,10 +5,20 @@ import "../../VotingInterface.sol";
 import "../../ValidOrOwned.sol";
 import "../../GDAO.sol";
 
+
+/*
+** This is a first implementation of a majority vote.
+** The simple majority voting rule increments the "voters" number for each vote.
+** The proposal is considered to be passed if it has at least (voters / 2 + 1) votes.
+*/
+
+
 contract SimpleMajorityVoting is VotingInterface, ValidOrOwned {
     uint voters = 0;
     mapping (address => uint) votes;
 
+
+// Constructor function is empty
     function SimpleMajorityVoting(GDAO _proxy) Valid(_proxy){
     }
 
@@ -26,9 +36,15 @@ contract SimpleMajorityVoting is VotingInterface, ValidOrOwned {
 			throw;
 		return (votes[proposalToCheck] > (voters / 2));
     }
-	
+
+
+// The two functions below are for informations purpose only.
+
 	function getVotersNumber() external returns (uint){
 		return (voters);
 	}
 
+	function getVotesRequiredToPass() external returns (uint){
+		return (voters / 2 + 1);
+	}
 }
