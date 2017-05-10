8c71d512cdeedbaa25bb9eb4b1f1df91c309bc98
AK42 <antokop@hotmail.com>
2017-05-10 13:12:55 +0200

    added some more tests

--- test/solidity/TestSimpleMajorityVoting.sol
+++ test/solidity/TestSimpleMajorityVoting.sol
@@ -32,26 +32,56 @@ contract TestSimpleMajorityVoting {
 		//	Init 2 dummy proposals.
 		var proposal = new DummyProposal(1);
 		var proposal2 = new DummyProposal(2);
-
+		
+		// Propose and vote twice.
 		newVoting.propose(proposal);
 		newVoting.vote(proposal);
 		newVoting.vote(proposal);
 		Assert.equal(newVoting.getVotersNumber(), 2, "Should have 2 voters");
 		Assert.equal(newVoting.getProposalVotes(proposal), 2, "Should have 2 voters");
-		newVoting.vote(proposal2);
-		Assert.equal(newVoting.getProposalVotes(proposal2), 0, "Should have 0 voters");
-		Assert.equal(newVoting.getVotersNumber(), 2, "Should have 2 voters");
+		
+		// New proposal and vote twice again
 		newVoting.propose(proposal2);
 		newVoting.vote(proposal2);
 		newVoting.vote(proposal2);
+
+		// Testing that we do have 4 total votes, 2 for each.
 		Assert.equal(newVoting.getVotersNumber(), 4, "Should have 2 voters");	
+		Assert.equal(newVoting.getProposalVotes(proposal), 2, "Should have 2 voters");
+		Assert.equal(newVoting.getProposalVotes(proposal2), 2, "Should have 2 voters");
 	}
 	
 	function testVoteForUnknown() {
+
+		// Init 1 dummy proposal.
 		var proposal = new DummyProposal(1);
+
+		// Try to vote before proposing it.
 		newVoting.vote(proposal);
+
+		// Testing that it didn't vote.
 		Assert.equal(newVoting.getVotersNumber(), 0, "Total Voters should be zero (unknown proposal)");
 		Assert.equal(newVoting.getProposalVotes(proposal), 0, "Total votes for proposal should be zero (unknown proposal)");
+	}
+
+	function testVoteAndGetWinner() {
+		//	Init 2 dummy proposals.
+		var proposal = new DummyProposal(1);
+		var proposal2 = new DummyProposal(2);
+		
+		// Propose both and vote twice for 1st, once for 2nd.
+		newVoting.propose(proposal);
+		newVoting.propose(proposal2);
+		newVoting.vote(proposal);
+		newVoting.vote(proposal);
+		newVoting.vote(proposal2);
+
+		// Test number of votes required to pass.
+		Assert.equal(newVoting.getVotesRequiredToPass(), 2, "Total votes required to pass should be 2 (3/2+1)");
+
+		Assert.isFalse(newVoting.isPassed(proposal2), "Proposal2 should not be passed");
+		Assert.isTrue(newVoting.isPassed(proposal), "Proposal should be passed");
+	
 	}
 
 }
